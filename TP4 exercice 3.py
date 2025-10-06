5f97582dbae08346218dbaf87c644615233f4ee9
voisinc <voisinc@educ.college-montreal.qc.ca>
2025-10-06 14:43:45 -0400

    Initial commit

---
+++ TP4 exercice 3.py
@@ -0,0 +1,11 @@
+from math import pi
+
+class Cercle:
+    def __init__(self, rayon):
+        self.rayon = ""
+        self.aire = pi*rayon**2
+        self.circo = 2*pi*rayon
+
+donnee = Cercle(789)
+print(f"L'aire du cercle est de {donnee.aire} cm2")
+print(f"La circonférence du cercle est de {donnee.circo}cm2")
